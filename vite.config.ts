f8ac228235e925b763d6c1bbca830f7e8638f156
Joan Leon <joan.leon@gmail.com>
2024-11-06 16:03:43 +0100

    fix: fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import dts from "vite-plugin-dts";
-import { fileURLToPath } from "url";
-import { dirname, resolve } from "path";
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react(), dts()],
   root: "./demo",
   build: {
-    outDir: "../demo/dist",
+    outDir: "../dist",
     sourcemap: true,
     emptyOutDir: true,
   },
   resolve: {
     alias: {
-      "taskflow-defer": resolve(__dirname, "src"),
+      "taskflow-defer": "/src",
     },
   },
   test: {
